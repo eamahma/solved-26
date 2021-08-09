fbe2455444349bc6d1237dbfee2c0ae1a9739dbc
eamahma <eamahma@gmail.com>
2021-08-08 21:59:08 -0400

    updated path

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for feedback page
 app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+  res.sendFile(path.join(__dirname, './public/pages/feedback.html'))
 );
 
 // app.listen(PORT, () =>
